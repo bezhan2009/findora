718a769a9c8cad83ac13bc21fbaf4e9740e432bb
Бежан Каримов <karimovbezan0@gmail.com>
2025-09-14 18:20:53 +0000

    Try fixing this error: `Build Error: Error: x Expected '}', got '<eof>'.

--- src/hooks/use-favorites.ts
+++ src/hooks/use-favorites.ts
@@ -6,4 +6,7 @@ import { FavoritesContext } from '@/context/favorites-provider';
 export const useFavorites = () => {
   const context = useContext(FavoritesContext);
   if (context === undefined) {
-    
+    throw new Error('useFavorites must be used within a FavoritesProvider');
+  }
+  return context;
+};
